5ee6e38cb624bc750c19730356f31ab3d97e920a
alex <bigbadmath@gmail.com>
2021-06-12 22:28:57 -0400

    using clap to parse opts

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ use std::error::Error;
 use std::fs;
 use std::process;
 
-// (Full example with detailed comments in examples/01d_quick_example.rs)
-//
-// This example demonstrates clap's full 'custom derive' style of creating arguments which is the
-// simplest method of use, but sacrifices some flexibility.
 use clap::{AppSettings, Clap};
 use j4rs::{ClasspathEntry, InvocationArg, Jvm, JvmBuilder};
 use serde_json::Value;
@@ -25,75 +21,59 @@ use tantivy::Score;
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
 #[derive(Clap)]
-#[clap(version = "1.0", author = "Alex MN. <moore.niemi@gmail.com>")]
+#[clap(version = "0.1.1", author = "Alex MN. <moore.niemi@gmail.com>")]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short, long, default_value = "default.conf")]
-    config: String,
-    /// Some input. Because this isn't an Option<T> it's required to be used
+    /// The location of the Elasticsearch Lucene index
+    #[clap(short, long, default_value = "tests/resources")]
     input: String,
-    /// A level of verbosity, and can be used multiple times
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: i32,
-    #[clap(subcommand)]
-    subcmd: SubCommand,
+    /// The location of the Tantivy output index
+    #[clap(short, long, default_value = "/tmp/lucky/tantivy-idx")]
+    output: String,
+    /// To test that the docs still match, this is sent in as test query
+    #[clap(short, long, default_value = "lint")]
+    test_query: String,
 }
 
-#[derive(Clap)]
-enum SubCommand {
-    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
-    Test(Test),
-}
-
-/// A subcommand for controlling testing
-#[derive(Clap)]
-struct Test {
-    /// Print debug info
-    #[clap(short)]
-    debug: bool,
-}
-
-fn run() -> Result<(), Box<Error>> {
+fn run(input: String, output: String, test_query: String) -> Result<(), Box<Error>> {
     // destination index details
     let mut schema_builder = Schema::builder();
-    // fields
+    // TODO: this is the annoying part, we need to map schema to schema
     let title = schema_builder.add_text_field("title", TEXT | STORED);
     let content = schema_builder.add_text_field("content", TEXT | STORED);
-    let last_updated = schema_builder.add_date_field("last_updated", INDEXED);
-    // This is ES specific in a sense
+    // TODO: handling time means bringing in chrono etc
+    let _last_updated = schema_builder.add_date_field("last_updated", INDEXED);
+    // This is ES specific in that ES uses StoredField to contain entire JSON doc that was stored
     let source = schema_builder.add_text_field("source", STORED);
 
     let schema = schema_builder.build();
     dbg!(&schema);
 
     // safe to rm -rf /tmp/lucky/tantivy-idx
-    let index_path = "/tmp/lucky/tantivy-idx";
-    fs::create_dir_all(index_path)?;
+    fs::create_dir_all(&output)?;
 
-    let directory = MmapDirectory::open(&index_path)?;
+    let directory = MmapDirectory::open(&output)?;
     let index = Index::open_or_create(directory, schema.clone())?;
 
     let entry = ClasspathEntry::new("./java_wrapper/target/lucky-java-1.0-SNAPSHOT.jar");
     let jvm: Jvm = JvmBuilder::new()
         .classpath_entry(entry)
         .build()
-        .expect("can build JVM");
+        .expect("The system must support compatible JVM.");
 
-    let instantiation_args = vec![InvocationArg::try_from("tests/resources/")?];
+    // The ShardReader class takes a path to an index and can return a batching iterator of doc _source
+    let instantiation_args = vec![InvocationArg::try_from(input)?];
     let instance = jvm.create_instance("org.lucky.ShardReader", instantiation_args.as_ref())?;
-
     let chain = jvm.chain(&instance)?;
     let iterator = chain.invoke("batches", &[])?;
 
-    // Indexing documents
+    // # Indexing documents
 
-    // Here we use a buffer of 100MB that will be split
-    // between indexing threads.
+    // Tantivy buffer of 100MB that will be split between indexing threads.
     let mut index_writer = index.writer(100_000_000)?;
 
     // FIXME: tantivy DocId is incremental so this creates duplication on each run
-    // FIXME: see also https://docs.rs/tantivy/0.15.0/tantivy/type.DocId.html
+    //        see also https://docs.rs/tantivy/0.15.0/tantivy/type.DocId.html
     while iterator.invoke("hasNext", &[])?.to_rust()? {
         let batch: Vec<String> = iterator.invoke("next", &[])?.to_rust()?;
         batch.iter().for_each(|doc_source| {
@@ -114,6 +94,7 @@ fn run() -> Result<(), Box<Error>> {
         });
     }
 
+    // Like Lucene, Tantivy has to close writers before opening readers
     index_writer.commit()?;
 
     // # Searching
@@ -125,7 +106,7 @@ fn run() -> Result<(), Box<Error>> {
 
     let query_parser = QueryParser::for_index(&index, vec![title, content]);
 
-    let query = query_parser.parse_query("lint")?;
+    let query = query_parser.parse_query(&test_query)?;
     dbg!(&query);
 
     let top_docs: Vec<(Score, DocAddress)> = searcher.search(&query, &TopDocs::with_limit(10))?;
@@ -141,8 +122,10 @@ fn run() -> Result<(), Box<Error>> {
 }
 
 fn main() {
+    let opts: Opts = Opts::parse();
+
     // FIXME: this is old Rust 2015 format...
-    if let Err(e) = run() {
+    if let Err(e) = run(opts.input, opts.output, opts.test_query) {
         println!("Application error: {}", e);
         process::exit(1);
     }
